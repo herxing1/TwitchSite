f9c94eaa169f6432035fd0834d8bd7a824b310e2
herxing1 <herxing.g@gmail.com>
2026-09-25 17:33:52 +0200

    Ajouter le prochain live

---
+++ public/contenu.js
@@ -0,0 +1,26 @@
+window.SALON_CONTENT = {
+  "version": 1,
+  "site": {
+    "name": "herxing_",
+    "twitch": "herxing_",
+    "tagline": "On se retrouve en live.",
+    "description": "Le programme, les jeux et les rendez-vous de la chaîne. Tout pour se retrouver au même endroit.",
+    "about": "",
+    "timezone": "Europe/Paris",
+    "links": [],
+    "setup": []
+  },
+  "streams": [
+    {
+      "id": "6a6e6961-462d-449a-9e84-09e924c598cb",
+      "title": "La Reprise",
+      "start": "2026-09-25T19:00:00.000Z",
+      "end": "2026-09-25T22:00:00.000Z",
+      "game": "Just Chatting",
+      "description": "On parle tranquille pour biens reprendre hehe",
+      "gameRef": "category:just-chatting",
+      "gameImage": "images/just-chatting.jpg"
+    }
+  ],
+  "events": []
+};
